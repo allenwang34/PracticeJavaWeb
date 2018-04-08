91c0223691a999985d9d641ff619f9b70a8d300b
Yiyang Wang Mac <allenwang950321@gmail.com>
2018-04-08 15:31:42 +0800

    ProductServlet created

--- CartDemo/src/com/allen/cart/filter/CharSetEncodingFilter.java
+++ CartDemo/src/com/allen/cart/filter/CharSetEncodingFilter.java
@@ -4,17 +4,30 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
+
+/**
+*
+*
+* 字符编码过滤器
+*
+ */
+
 @WebFilter(filterName = "CharSetEncodingFilter")
 public class CharSetEncodingFilter implements Filter {
+
+    private String encoding;
+
     public void destroy() {
+
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        req.setCharacterEncoding(encoding);
         chain.doFilter(req, resp);
     }
 
     public void init(FilterConfig config) throws ServletException {
-
+        this.encoding=config.getInitParameter("encoding");
     }
 
 }
---
+++ CartDemo/src/com/allen/cart/servlet/ProductServlet.java
@@ -0,0 +1,26 @@
+package com.allen.cart.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ *
+ * 课程（商品）控制器
+ *
+ */
+
+@WebServlet(name = "ProductServlet")
+public class ProductServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
